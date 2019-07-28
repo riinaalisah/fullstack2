07bb756a13adc635625e61d44c956429509a327e
Riina-Alisa <riina.alisa@live.com>
2019-07-28 18:22:40 +0300

    trying again

---
+++ osa3/build/precache-manifest.c6d9bc222c7243d9309dcc95b5d85125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e09c0b6d154c9dc30891942fad7690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d1cd9283506f3371fd",
+    "url": "/static/css/main.90a86f1d.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "b8d1cd9283506f3371fd",
+    "url": "/static/js/main.53f211e0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
