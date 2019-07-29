f2166ab9afe078c3d0cdc3bc419fd23d62df78ec
Riina-Alisa <riina.alisa@live.com>
2019-07-29 10:47:21 +0300

    tehtävät 1, 2

---
+++ osa4/utils/middleware.js
@@ -0,0 +1,17 @@
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint '})
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+  
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+  
+  next(error)
+}
+
+module.exports = { unknownEndpoint, errorHandler }
